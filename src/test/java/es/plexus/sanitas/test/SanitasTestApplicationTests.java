785a249e37832fa016d9cfbf5c0496c83fe6c15f
Ruben civeira <ruben.civeiraiglesias@plexus.es>
2022-09-23 11:48:29 +0200

    plantilla inicial

---
+++ src/test/java/es/plexus/sanitas/test/SanitasTestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.plexus.sanitas.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanitasTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
